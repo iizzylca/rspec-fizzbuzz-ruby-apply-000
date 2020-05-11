8cca0a84a6c9c798e84fb06fae9aebda75fd96cc
Israel Canessa <israellcanessa@gmail.com>
2020-05-11 14:05:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,11 @@ fizz_3 = fizzbuzz(3)
 fizz_5 = fizzbuzz(5)
 fizz_15 = fizzbuzz (15)
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int_3 % 3 == 0
     "Fizz"
+  else int_5 % 5 == 0
+    "Buzz"
+  else int_15 % 5 ==0
+    "FizzBuzz"
   end
 end
